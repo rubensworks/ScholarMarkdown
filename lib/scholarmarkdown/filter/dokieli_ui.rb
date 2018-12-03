3b774f0640b103b11bd24ef08486f7857d5f2bbb
Ruben Taelman <ruben.taelman@ugent.be>
2018-12-03 12:30:44 +0100

    Update Dokieli UI imports

--- lib/scholarmarkdown/filter/dokieli_ui.rb
+++ lib/scholarmarkdown/filter/dokieli_ui.rb
@@ -6,12 +6,8 @@ Nanoc::Filter.define(:scholar_dokieli_ui) do |content|
   content.gsub! %r{\<\/head\>} do |match|
     <<-HTML
   <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono" rel="stylesheet" />
-  <link href="https://dokie.li/media/css/do.css" media="all" rel="stylesheet" />
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" media="all" rel="stylesheet" />
-  <script src="https://dokie.li/scripts/simplerdf.js"></script>
-  <script src="https://dokie.li/scripts/medium-editor.min.js"></script>
-  <script src="https://dokie.li/scripts/medium-editor-tables.min.js"></script>
-  <script src="https://dokie.li/scripts/do.js"></script>
+  <link href="https://dokie.li/media/css/dokieli.css" media="all" rel="stylesheet" />
+  <script src="https://dokie.li/scripts/dokieli.js"></script>
 </head>
     HTML
   end
