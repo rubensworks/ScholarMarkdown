5fc46111a01647b2c453aa1bd861a84591dc1790
Ruben Taelman <ruben.taelman@ugent.be>
2019-07-29 16:02:25 +0200

    Also mark author as schema:publisher

--- lib/scholarmarkdown/snippets.rb
+++ lib/scholarmarkdown/snippets.rb
@@ -35,6 +35,6 @@ def person name, website, profile, mainAuthor = true
   elsif not profile
     %{<a href="#{h website}">#{h name}</a>}
   else
-    %{<a property="foaf:maker schema:creator schema:author" href="#{h website}" typeof="foaf:Person schema:Person" resource="#{profile}">#{h name}</a>}
+    %{<a property="foaf:maker schema:creator schema:author schema:publisher" href="#{h website}" typeof="foaf:Person schema:Person" resource="#{profile}">#{h name}</a>}
   end
 end
